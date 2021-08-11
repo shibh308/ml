fc5027f50783d27e3693ed511145956d2f3f3bba
Shibata Hiroki <s17097@tokyo.kosen-ac.jp>
2021-08-11 17:18:19 +0900

    [add]gen sample

--- gan-impl/train.py
+++ gan-impl/train.py
@@ -93,6 +93,9 @@ def main(d1, g1, g2):
 
         print('epoch: {:3d}, d_loss:{:.3f}, gen_loss:{:.3f}'.format(epoch + 1, d_loss_sum / len(train_loader), g_loss_sum / len(train_loader)))
 
+        noise = torch.randn(10, 128).to(device)
+        g_gen = generator(noise).reshape(-1, 28, 28).to('cpu').detach().numpy() * 256
+
         for idx in range(len(g_gen)):
             out_img = g_gen[idx].astype(np.int32)
             img_path = os.path.join('../results/gan-impl', start_time, 'gen_{}_{}.png'.format(epoch, idx))
